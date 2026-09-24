b8e69ff9fe2826d288e6c73c4631b49ecf7d60a8
Kp344 <149116840+Kp344@users.noreply.github.com>
2026-09-24 17:14:43 -0400

    Update script.js

--- Egg-Drop-Challenge/Egg-Drop-Challenge-V4/script.js
+++ Egg-Drop-Challenge/Egg-Drop-Challenge-V4/script.js
@@ -181,7 +181,7 @@ const DEFAULT_CONTENT = {
             {
               "type": "embed",
               "src": "https://youtube.com/shorts/YZT3ifzZXyg?feature=share",
-              "shape": "wide",
+              "shape": "square",
               "caption": "Prototype test",
               "span": "narrow"
             }
@@ -201,7 +201,7 @@ const DEFAULT_CONTENT = {
             {
               "type": "embed",
               "src": "https://youtube.com/shorts/aUqE639X2QQ?feature=share",
-              "shape": "wide",
+              "shape": "square",
               "caption": "Final Product test",
               "span": "narrow"
             }
